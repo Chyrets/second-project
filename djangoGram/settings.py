adb997a3236892a5458c7519f20555349912d845
chyrets <uladzislawchyrets@gmail.com>
2021-05-17 13:29:03 +0300

    connected postgresql

--- djangoGram/settings.py
+++ djangoGram/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'djangoGram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoGram_db',
+        'USER': 'djangoGram_user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
